e504b27718d78e8b015128cdd6b1f636dcce2403
Sharonsyra <sharonwaithii@gmail.com>
2018-02-14 14:17:50 +0300

    fix: Fix packaging bug

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from app import db, create_app
+from app.requests.views import db, create_app
 
 app = create_app(config_name=os.getenv('APP_SETTINGS'))
 migrate = Migrate(app, db)
